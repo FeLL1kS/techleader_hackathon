7997e2a407c613c36318b9971d1d0e6f40b14809
FeLL1kS <oleg5420@yandex.ru>
2023-10-11 20:41:31 +0300

    typescript + eslint initialisation

---
+++ .eslintrc.js
@@ -0,0 +1,185 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // uses typescript-specific linting rules
+    'plugin:prettier/recommended', // enables eslint-plugin-prettier and eslint-config-prettier
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['import', '@typescript-eslint'],
+  rules: {
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'throw',
+      },
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+    'no-useless-catch': ['error'],
+    'func-names': ['off'],
+    'no-duplicate-imports': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-types': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/consistent-type-assertions': 'error',
+    '@typescript-eslint/consistent-type-definitions': 'error',
+    '@typescript-eslint/indent': [
+      'off',
+      4,
+      {
+        FunctionDeclaration: {
+          parameters: 'first',
+        },
+        FunctionExpression: {
+          parameters: 'first',
+        },
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    // allow use @ts-ignore
+    '@typescript-eslint/ts-ignore': 'off',
+    // do not show warning on Example 'ctx.params.kpiComponent?.toDelete!'
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    // do not show warning if 'Missing return type on function'
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // do not show warning on Example 'toDelete!'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-var-requires': 'error',
+    "@typescript-eslint/no-shadow": 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-function-type': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/triple-slash-reference': 'error',
+    '@typescript-eslint/type-annotation-spacing': 'error',
+    '@typescript-eslint/unified-signatures': 'error',
+    'arrow-body-style': 'error',
+    'arrow-parens': ['error', 'as-needed'],
+    camelcase: 'error',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    complexity: 'off',
+    'constructor-super': 'error',
+    curly: 'error',
+    'dot-notation': 'error',
+    'eol-last': 'error',
+    eqeqeq: ['error', 'smart'],
+    'guard-for-in': 'error',
+    'id-blacklist': ['error', 'callback', 'cb'],
+    'id-match': 'error',
+    // "import/order": "error",
+    'max-classes-per-file': ['error', 1],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+      },
+    ],
+    'new-parens': 'error',
+    'no-bitwise': 'error',
+    'no-caller': 'error',
+    'no-cond-assign': 'error',
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-empty': 'error',
+    'no-eval': 'error',
+    'no-fallthrough': 'off',
+    'no-invalid-this': 'off',
+    'no-multiple-empty-lines': 'error',
+    'no-new-wrappers': 'error',
+    'no-shadow': 'off',
+    'no-throw-literal': 'error',
+    'no-trailing-spaces': 'error',
+    'no-undef-init': 'error',
+    // "no-underscore-dangle": "error",
+    'no-unsafe-finally': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-labels': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'one-var': ['error', 'never'],
+    'prefer-const': 'error',
+    'quote-props': ['error', 'as-needed'],
+    radix: 'error',
+    'space-before-function-paren': [
+      'error',
+      {
+        // to remove prettier <-> eslint conflict. No option in prettier to change this.
+        anonymous: 'always',
+        asyncArrow: 'always',
+        named: 'never',
+      },
+    ],
+    'spaced-comment': 'error',
+    'use-isnan': 'error',
+    'valid-typeof': 'off',
+  },
+};
